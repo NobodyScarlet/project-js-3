8997be9e2a9c658e8eeb672fe9437a0ffd74e294
Yevhenii <89587950+NobodyScarlet@users.noreply.github.com>
2022-03-21 18:52:52 +0200

    up

---
+++ js/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Task 1
+
+let admin;
+let name;
+
+name = 'John';
+
+admin = name;
+
+alert(admin)
+
+// Task 2
+
+let planetName;
+let currentUserName;
+
+// Task 3
+
+let value = "10" + 10; // string + number
+
+console.log(typeof value);
+
